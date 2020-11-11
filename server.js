b8f736d13d6bc68a1300ff0ec7a55b3ba4363454
Atul1023 <07atul1023@gmail.com>
2020-11-11 17:52:27 +0530

    added database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-regular-36614",
-    user: "postgres",
-    password: "Rishi@007",
-    database: "smartbrain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
